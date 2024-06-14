c44a5c847c5fcec268ddde5cb91d0234ef8a11eb
Kristina Tripak <tinatripak2002@gmail.com>
2024-06-14 10:51:49 -0400

    added Analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Analytics />
       <App />
     </Provider>
   </React.StrictMode>
